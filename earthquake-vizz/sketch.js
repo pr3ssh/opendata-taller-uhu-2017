a1db5feb880a6f7d817af1b950b50f0f5b2307de
pr3ssh <pablo.martin@enreda.coop>
2017-04-14 13:48:50 +0200

    earthquake-vizz (step 3)

--- earthquake-vizz/sketch.js
+++ earthquake-vizz/sketch.js
@@ -1,6 +1,9 @@
 /*
- * Basecode from Daniel Shiffman
+ * Codebase from Daniel Shiffman
  * https://github.com/CodingTrain/Rainbow-Code/blob/master/challenges/CC_57_Earthquake_Viz/sketch.js
+ *
+ * DATA from https://earthquake.usgs.gov/earthquakes/feed/v1.0/csv.php
+ *
  */
 
 var mapimg;
@@ -13,12 +16,15 @@ var hh = 512;
 
 var zoom = 1;
 
+var earthquakes;
+
 
 function preload() {
     mapimg = loadImage('https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/' +
         clat + ',' + clon + ',' + zoom + '/' +
         ww + 'x' + hh +
         '?access_token=pk.eyJ1IjoiZWRpcG90cmVib2wiLCJhIjoid25OZHV1OCJ9.C4l-Uqhel8nrTRvY3cUY2w');
+    earthquakes = loadStrings('data/all_month.csv');
 }
 
 
