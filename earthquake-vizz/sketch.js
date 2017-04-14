9a7ccde7f9b85314cd518f407bcf377129a0051d
pr3ssh <pablo.martin@enreda.coop>
2017-04-14 14:36:04 +0200

    earthquake-vizz (step 4)

--- earthquake-vizz/sketch.js
+++ earthquake-vizz/sketch.js
@@ -4,6 +4,8 @@
  *
  * DATA from https://earthquake.usgs.gov/earthquakes/feed/v1.0/csv.php
  *
+ * MAP DATA PROJECTION on https://en.wikipedia.org/wiki/Web_Mercator
+ *
  */
 
 var mapimg;
@@ -28,12 +30,43 @@ function preload() {
 }
 
 
+function mercX(lon) {
+    lon = radians(lon);
+    var a = (256 / PI) * pow(2, zoom);
+    var b = lon + PI;
+    return a * b;
+}
+
+function mercY(lat) {
+    lat = radians(lat);
+    var a = (256 / PI) * pow(2, zoom);
+    var b = tan(PI / 4 + lat / 2);
+    var c = PI - log(b);
+    return a * c;
+}
+
+
 function setup() {
     createCanvas(ww, hh);
     background(20, 20, 20);
     translate(width / 2, height / 2);
     imageMode(CENTER);
     image(mapimg, 0, 0);
+
+    var cx = mercX(clon);
+    var cy = mercY(clat);
+
+    for (var i = 1; i < earthquakes.length; i++) {
+	var data = earthquakes[i].split(/,/);
+	var lat = data[1];
+	var lon = data[2];
+	var x = mercX(lon) - cx;
+	var y = mercY(lat) - cy;
+	var d = 2; 
+        noStroke();
+	fill(255, 0, 255, 200);
+	ellipse(x, y, d, d);
+    }
 }
 
 
