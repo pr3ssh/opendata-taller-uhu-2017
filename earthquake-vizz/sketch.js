a56857c2440bf4ddb64c81966658ee703653ad28
pr3ssh <pablo.martin@enreda.coop>
2017-04-14 13:22:17 +0200

    earthquake-vizz (step 1)

---
+++ earthquake-vizz/sketch.js
@@ -0,0 +1,14 @@
+/*
+ * Basecode from Daniel Shiffman
+ * https://github.com/CodingTrain/Rainbow-Code/blob/master/challenges/CC_57_Earthquake_Viz/sketch.js
+ */
+
+var ww = 1024;
+var hh = 512;
+
+function setup() {
+    createCanvas(ww, hh);
+    background(20, 20, 20);
+}
+
+function draw() {}
